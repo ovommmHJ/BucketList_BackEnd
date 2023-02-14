9e6d615449f29dbead6a8f2ae19dbdba0fa4428c
ovommmhj <killos033@gmail.com>
2023-02-14 11:40:53 +0900

    feat :: 세션관련 설정 진행중

--- src/main/java/com/bucket/service/BucketService.java
+++ src/main/java/com/bucket/service/BucketService.java
@@ -5,9 +5,12 @@ import com.bucket.dao.MemberRepository;
 import com.bucket.entity.Bucket;
 import com.bucket.entity.Member;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -41,9 +44,22 @@ public class BucketService {
         return result;
     }
 
-    public List<Bucket> getPickBucketList(HttpServletRequest request) {
-        List<Bucket> list = bucketRepo.findAll();
 
-        return list
+    public List<Bucket> getOwnBucketList(HttpServletRequest request) {
+
+        HttpSession se = request.getSession();
+        Member member = (Member) se.getAttribute("member");
+
+        List<Bucket> list = new ArrayList<Bucket>();
+        if(member != null){
+            String id = member.getId();
+             list = bucketRepo.findById(id);
+        }
+
+        for(Bucket b : list){
+            System.out.println(b.getTitle());
+        }
+
+        return list;
     }
 }
